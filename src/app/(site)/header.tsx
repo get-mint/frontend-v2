7ac323d140871ad71e8896cf6bf53ddbb7e9d4a7
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-15 22:09:15 -0500

    refactor: Enhance mobile navigation in Header component by implementing a dropdown menu and adjusting layout for better user experience

--- src/app/(site)/header.tsx
+++ src/app/(site)/header.tsx
@@ -36,7 +36,7 @@ export function Header() {
         </Link>
 
         <div className="flex items-center gap-3 md:gap-6 lg:gap-8">
-          {!isMobile ? (
+          {!isMobile && (
             <nav className="flex gap-6">
               {items.map((item) => (
                 <Link
@@ -48,32 +48,15 @@ export function Header() {
                 </Link>
               ))}
             </nav>
-          ) : (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className="w-10 h-10">
-                  <MenuIcon className="size-5" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                {items.map((item) => (
-                  <DropdownMenuItem key={item.href} asChild>
-                    <Link href={item.href} className="cursor-pointer">
-                      {item.label}
-                    </Link>
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            </DropdownMenu>
           )}
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-3">
             {!user ? (
               <>
                 <Link href="/auth/login" passHref>
                   <Button variant="outline">Log In</Button>
                 </Link>
-                
+
                 <Link href="/auth/signup" passHref>
                   <Button className="font-semibold">Join Mint</Button>
                 </Link>
@@ -82,6 +65,27 @@ export function Header() {
               <Button variant="outline" size="icon" className="w-10 h-10">
                 <UserIcon className="size-5" />
               </Button>
+            )}
+
+            {isMobile && (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" size="icon" className="w-10 h-10">
+                    <MenuIcon className="size-5" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-32 mr-4">
+                  {items.map((item) => (
+                    <DropdownMenuItem
+                      key={item.href}
+                      asChild
+                      className="text-lg font-semibold transition-all cursor-pointer"
+                    >
+                      <Link href={item.href}>{item.label}</Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
             )}
           </div>
         </div>
