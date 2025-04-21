28624c03da86fdeaa13b36729c6fcfedfe1bb82d
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-20 23:42:06 -0500

    refactor: Replace pageTitles object with items array for user layout, enhancing readability and maintainability

--- src/app/(site)/user/layout.tsx
+++ src/app/(site)/user/layout.tsx
@@ -4,11 +4,20 @@ import { usePathname } from "next/navigation";
 
 import { CurrencySelect } from "@/components/currency/currency-select";
 
-const pageTitles: Record<string, string> = {
-  "/user": "Account",
-  "/user/activity": "Activity",
-  "/user/affiliate": "Refer & Earn",
-};
+const items: { label: string; href: string }[] = [
+  {
+    label: "Account",
+    href: "/user",
+  },
+  {
+    label: "Activity",
+    href: "/user/activity",
+  },
+  {
+    label: "Refer & Earn",
+    href: "/user/affiliate",
+  },
+];
 
 export default function UserLayout({
   children,
@@ -16,12 +25,14 @@ export default function UserLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  const pageTitle = pageTitles[pathname] || "Dashboard";
+
+  const item = items.find((item) => item.href === pathname);
 
   return (
     <div className="container px-6 py-6 mx-auto max-w-7xl">
-      <div className="flex flex-col items-start justify-between gap-4 mb-6 md:flex-row md:items-center">
-        <h1 className="text-3xl font-bold">{pageTitle}</h1>
+      <div className="flex flex-col justify-between gap-4 mb-6 items-starts md:flex-row md:items-end">
+        <h1 className="text-3xl font-bold">{item?.label}</h1>
+
         <CurrencySelect triggerClassName="w-[180px]" />
       </div>
 
