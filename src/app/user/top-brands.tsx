5d8189a8eb34d4f33c8d2577b18cd601ee1de95b
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-29 15:19:55 -0500

    refactor: Simplify TopBrands component by integrating data fetching directly within useEffect and utilizing user-selected currency for improved responsiveness

--- src/app/user/top-brands.tsx
+++ src/app/user/top-brands.tsx
@@ -9,36 +9,36 @@ import { Tables } from "@/types/supabase";
 
 import { Brands } from "./brands";
 
-async function getBrands(currencyId?: number) {
-  const supabase = createClient();
-
-  let query = supabase
-    .from("brands")
-    .select("*")
-    .order("priority", { ascending: true })
-    .limit(10);
-
-  if (currencyId) {
-    query = query.eq("currency_id", currencyId);
-  }
-
-  const { data, error } = await query;
-
-  if (error) {
-    throw error;
-  }
-
-  return data as Tables<"brands">[];
-}
-
 export function TopBrands() {
   const { user } = useAuth();
 
   const [brands, setBrands] = useState<Tables<"brands">[]>([]);
 
   useEffect(() => {
-    getBrands(user?.selected_currency_id).then(setBrands);
-  }, [user?.selected_currency_id]);
+    const getBrands = async () => {
+      const supabase = createClient();
+
+      let query = supabase
+        .from("brands")
+        .select("*")
+        .order("priority", { ascending: true })
+        .limit(10);
+
+      if (user?.selected_currency_id) {
+        query = query.eq("currency_id", user?.selected_currency_id);
+      }
+
+      const { data, error } = await query;
+
+      if (error) {
+        throw error;
+      }
+
+      setBrands(data);
+    };
+
+    getBrands();
+  }, []);
 
   return (
     <div className="flex flex-col gap-6">
