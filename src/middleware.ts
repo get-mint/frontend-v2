9b0c3203adb54ba8b156612decdb9b34c929bc4f
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-29 15:09:10 -0500

    feat: Add redirect from root path to home in middleware for improved user navigation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 import { updateSession } from "@/lib/supabase/server/middleware";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname == "/") {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+
   return await updateSession(request);
 }
 
