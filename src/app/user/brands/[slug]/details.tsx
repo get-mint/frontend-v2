4e65685c1aa87b20e1a99b1d2d758988a8e2b5a8
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-29 21:01:29 -0500

    refactor: Rearrange cashback offers display in Details component for better organization and clarity

--- src/app/user/brands/[slug]/details.tsx
+++ src/app/user/brands/[slug]/details.tsx
@@ -14,38 +14,29 @@ export function Details({ brand }: { brand: BrandAndNetworkAndOffers }) {
         <p className="mb-3 text-lg font-medium text-justify">
           {brand.description}
         </p>
-
-        <h1 className="text-3xl font-bold">Cashback Offers</h1>
-        <p className="text-lg font-medium text-justify">
-          We've found {brand.offers.filter((offer) => offer.commission > 0).length} cashback offers for {brand.name}.
-        </p>
-        <div className="flex flex-wrap gap-3">
-          {brand.offers
-            .filter((offer) => offer.commission > 0)
-            .map((offer) => (
-              <div
-                key={offer.description}
-                className="flex flex-col justify-center gap-2 px-6 py-4 transition-all bg-muted rounded-xl max-w-96 hover:ring-2 hover:ring-primary"
-              >
-                <h1 className="text-xl font-bold">
-                  <span className="text-primary">
-                    {offer.commission / 2}
-                    {offer.type === "sale"
-                      ? "%"
-                      : selectedCurrency?.symbol}{" "}
-                  </span>
-                  cashback
-                </h1>
-                <p className="text-sm font-medium text-muted-foreground">
-                  {offer.description}
-                </p>
-              </div>
-            ))}
-        </div>
       </div>
 
       <div className="flex flex-col flex-1 gap-3">
         <h1 className="text-3xl font-bold">Earn Cashback</h1>
+        {brand.offers
+          .filter((offer) => offer.commission > 0)
+          .map((offer) => (
+            <div
+              key={offer.description}
+              className="flex flex-col justify-center gap-2 px-6 py-4 transition-all bg-muted rounded-xl max-w-96 hover:ring-2 hover:ring-primary"
+            >
+              <h1 className="text-xl font-bold">
+                <span className="text-primary">
+                  {offer.commission / 2}
+                  {offer.type === "sale" ? "%" : selectedCurrency?.symbol}{" "}
+                </span>
+                cashback
+              </h1>
+              <p className="text-sm font-medium text-muted-foreground">
+                {offer.description}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );
