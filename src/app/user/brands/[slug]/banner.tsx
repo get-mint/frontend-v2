42114ccd7248b2411f4f00f3c0e284bd09c9ae13
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-29 20:33:21 -0500

    refactor: Enhance Banner and BrandPage components with improved navigation and loading state handling using Suspense

--- src/app/user/brands/[slug]/banner.tsx
+++ src/app/user/brands/[slug]/banner.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { BrandAndNetworkAndOffers } from "./types";
 
 export default function Banner({ brand }: { brand: BrandAndNetworkAndOffers }) {
+  const router = useRouter();
   return (
     <>
-      <h1 className="text-4xl font-extrabold text-center">
-        Earn up to {brand.offers[0].commission / 2}
-        {brand.offers[0].type === "sale" ? "%" : "€"} cashback on {brand.name}
-      </h1>
+      <div className="relative flex flex-row items-center justify-center">
+        <Button  variant="default" onClick={() => router.back()} className="absolute left-0">
+          <ArrowLeft />
+          Back
+        </Button>
+
+        <h1 className="text-4xl font-extrabold text-center">
+          Earn up to {brand.offers[0].commission / 2}
+          {brand.offers[0].type === "sale" ? "%" : "€"} cashback on {brand.name}
+        </h1>
+      </div>
 
       <div
         className="flex flex-col items-center justify-center w-full py-12 rounded-xl bg-muted"
--- src/app/user/brands/[slug]/page.tsx
+++ src/app/user/brands/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import { Suspense } from "react";
+
 import { Brand } from "./brand";
+import { Loader } from "@/components/ui/loader";
 
 export default async function BrandPage({
   params,
@@ -7,5 +10,15 @@ export default async function BrandPage({
 }) {
   const { slug } = await params;
 
-  return <Brand slug={slug} />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center w-full h-[80vh]">
+          <Loader className="size-16 text-primary" />
+        </div>
+      }
+    >
+      <Brand slug={slug} />
+    </Suspense>
+  );
 }
