8c26f5f1cb6df33801dd29e7ff4693a450ba6398
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-01 19:57:37 -0500

    refactor: Integrate Banner component into Refer and Earn page for improved layout and user engagement

--- src/app/user/refer-and-earn/page.tsx
+++ src/app/user/refer-and-earn/page.tsx
@@ -1,7 +1,15 @@
+import { Banner } from "./banner";
+
 export default function ReferAndEarn() {
   return (
     <>
-      <h1 className="text-3xl font-bold">Refer and Earn</h1>
+      <Banner />
+      <div className="flex flex-col gap-3">
+        <h1 className="text-3xl font-bold">Refer and Earn</h1>
+        <p className="text-lg font-medium text-muted-foreground">
+          Fix me
+        </p>
+      </div>{" "}
     </>
   );
 }
