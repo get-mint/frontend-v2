4bd6117fe3e07bf49d06f40e465b4cb57f477011
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-01 20:12:15 -0500

    feat: Implement affiliate fetching logic in Refer and Earn page to enhance user experience and display affiliate status

--- src/app/user/refer-and-earn/page.tsx
+++ src/app/user/refer-and-earn/page.tsx
@@ -1,15 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { useAuth } from "@/lib/hooks/use-auth";
+
+import { Loader } from "@/components/ui/loader";
+
 import { Banner } from "./banner";
+import { Tables } from "@/types/supabase";
+import { createClient } from "@/lib/supabase/client";
 
 export default function ReferAndEarn() {
-  return (
+  const { user } = useAuth();
+
+  const [loading, setLoading] = useState(true);
+  const [affiliate, setAffiliate] = useState<Tables<"affiliates"> | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (!user?.auth_user_id) return;
+    const fetchAffiliate = async () => {
+      setLoading(true);
+
+      const supabase = createClient();
+
+      const { data, error } = await supabase
+        .from("affiliates")
+        .select("*")
+        .eq("user_id", user?.auth_user_id)
+        .maybeSingle();
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      setAffiliate(data);
+
+      setLoading(false);
+    };
+
+    fetchAffiliate();
+  }, [user?.auth_user_id]);
+
+  return loading ? (
+    <div className="flex items-center justify-center h-[80vh]">
+      <Loader className="size-12 text-primary" />
+    </div>
+  ) : (
     <>
-      <Banner />
-      <div className="flex flex-col gap-3">
-        <h1 className="text-3xl font-bold">Refer and Earn</h1>
-        <p className="text-lg font-medium text-muted-foreground">
-          Fix me
-        </p>
-      </div>{" "}
+      {affiliate ? (
+        <div className="flex items-center justify-center h-[80vh]">
+          <p className="text-2xl font-bold">You are already an affiliate.</p>
+        </div>
+      ) : (
+        <Banner />
+      )}
     </>
   );
 }
