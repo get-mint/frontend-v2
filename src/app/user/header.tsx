3b523ce9d5f64246d392c7b6fa9ba914603df4f6
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-07 19:14:13 -0500

    refactor: Simplify UserHeader component by removing unused state and effects, enhancing code clarity and maintainability

--- src/app/user/header.tsx
+++ src/app/user/header.tsx
@@ -1,73 +1,20 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 
-import {
-  SearchIcon,
-  UserIcon,
-  HandCoinsIcon,
-  HistoryIcon,
-  LogOutIcon,
-  ChevronDownIcon,
-  LayoutDashboardIcon,
-} from "lucide-react";
-
-import { useAuth } from "@/lib/hooks/use-auth";
-import { useIsMobile } from "@/lib/hooks/use-mobile";
-import { createClient } from "@/lib/supabase/client";
+import { SearchIcon } from "lucide-react";
 
 import { Header } from "@/components/layout/header";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+
 
 export function UserHeader() {
   const router = useRouter();
 
-  const { user, selectedCurrency, logOut } = useAuth();
-  const isMobile = useIsMobile();
-
   const [search, setSearch] = useState<string>("");
-  const [balance, setBalance] = useState<number>(0);
 
-  useEffect(() => {
-    if (!user || !selectedCurrency) {
-      return;
-    }
-
-    const fetchBalance = async () => {
-      const supabase = createClient();
-
-      const { data, error } = await supabase
-        .from("user_balance_entries")
-        .select("balance")
-        .eq("user_id", user?.id)
-        .eq("currency_id", selectedCurrency?.id)
-        .order("created_at", { ascending: false })
-        .limit(1);
-
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      if (data.length === 0) {
-        setBalance(0);
-        return;
-      }
-
-      setBalance(data[0].balance);
-    };
-
-    fetchBalance();
-  }, [user, selectedCurrency]);
 
   const handleSearch = () => {
     router.push(`/user/brands?search=${search}`);
@@ -92,61 +39,7 @@ export function UserHeader() {
         </Button>
       </div>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <div className="flex flex-row items-center justify-center p-2 transition-all bg-white border rounded-full cursor-pointer hover:border-primary hover:ring-1 hover:ring-primary">
-            {!isMobile && (
-              <Button size="icon" className="mr-3">
-                <UserIcon className="size-5" />
-              </Button>
-            )}
-
-            <span className="mr-1 font-semibold">
-              {selectedCurrency?.symbol}
-              {balance.toFixed(2)}
-            </span>
-            <ChevronDownIcon className="mr-1 size-5" />
-          </div>
-        </DropdownMenuTrigger>
-
-        <DropdownMenuContent className="w-64 p-0 mt-1 rounded-t-none z-49">
-          <div className="p-4 border-b">
-            <span className="text-lg font-bold">My Account</span>
-          </div>
-
-          <Link href="/user/dashboard" passHref>
-            <DropdownMenuItem className="p-4 font-medium rounded-none cursor-pointer text-md">
-              <LayoutDashboardIcon className="size-5 text-foreground" />
-              Dashboard
-            </DropdownMenuItem>
-          </Link>
-
-          <Link href="/user/activity" passHref>
-            <DropdownMenuItem className="p-4 font-medium rounded-none cursor-pointer text-md">
-              <HistoryIcon className="size-5 text-foreground" />
-              Activity
-            </DropdownMenuItem>
-          </Link>
-
-          <Link href="/user/refer-and-earn" passHref>
-            <DropdownMenuItem className="p-4 font-medium rounded-none cursor-pointer text-md">
-              <HandCoinsIcon className="size-5 text-foreground" />
-              Refer & Earn
-            </DropdownMenuItem>
-          </Link>
-
-          <DropdownMenuItem
-            className="p-4 font-medium rounded-none cursor-pointer text-md text-destructive"
-            onClick={() => {
-              logOut();
-              router.push("/auth/login");
-            }}
-          >
-            <LogOutIcon className="size-5 text-destructive" />
-            Logout
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <UserHeader />
     </Header>
   );
 }
---
+++ src/app/user/user-header.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import {
+  UserIcon,
+  HandCoinsIcon,
+  HistoryIcon,
+  LogOutIcon,
+  ChevronDownIcon,
+  LayoutDashboardIcon,
+} from "lucide-react";
+
+import { useIsMobile } from "@/lib/hooks/use-mobile";
+import { useAuth } from "@/lib/hooks/use-auth";
+import { createClient } from "@/lib/supabase/client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+export function UserHeader() {
+  const router = useRouter();
+  const isMobile = useIsMobile();
+
+  const { user, selectedCurrency, logOut } = useAuth();
+
+  const [balance, setBalance] = useState<number>(0);
+
+  useEffect(() => {
+    if (!user || !selectedCurrency) {
+      return;
+    }
+
+    const fetchBalance = async () => {
+      const supabase = createClient();
+
+      const { data, error } = await supabase
+        .from("user_balance_entries")
+        .select("balance")
+        .eq("user_id", user?.id)
+        .eq("currency_id", selectedCurrency?.id)
+        .order("created_at", { ascending: false })
+        .limit(1);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      if (data.length === 0) {
+        setBalance(0);
+        return;
+      }
+
+      setBalance(data[0].balance);
+    };
+
+    fetchBalance();
+  }, [user, selectedCurrency]);
+
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="flex flex-row items-center justify-center p-2 transition-all bg-white border rounded-full cursor-pointer hover:border-primary hover:ring-1 hover:ring-primary">
+          {!isMobile && (
+            <Button size="icon" className="mr-3">
+              <UserIcon className="size-5" />
+            </Button>
+          )}
+
+          <span className="mr-1 font-semibold">
+            {selectedCurrency?.symbol}
+            {balance.toFixed(2)}
+          </span>
+          <ChevronDownIcon className="mr-1 size-5" />
+        </div>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-64 p-0 mt-1 rounded-t-none z-49">
+        <div className="p-4 border-b">
+          <span className="text-lg font-bold">My Account</span>
+        </div>
+
+        <Link href="/user/dashboard" passHref>
+          <DropdownMenuItem className="p-4 font-medium rounded-none cursor-pointer text-md">
+            <LayoutDashboardIcon className="size-5 text-foreground" />
+            Dashboard
+          </DropdownMenuItem>
+        </Link>
+
+        <Link href="/user/activity" passHref>
+          <DropdownMenuItem className="p-4 font-medium rounded-none cursor-pointer text-md">
+            <HistoryIcon className="size-5 text-foreground" />
+            Activity
+          </DropdownMenuItem>
+        </Link>
+
+        <Link href="/user/refer-and-earn" passHref>
+          <DropdownMenuItem className="p-4 font-medium rounded-none cursor-pointer text-md">
+            <HandCoinsIcon className="size-5 text-foreground" />
+            Refer & Earn
+          </DropdownMenuItem>
+        </Link>
+
+        <DropdownMenuItem
+          className="p-4 font-medium rounded-none cursor-pointer text-md text-destructive"
+          onClick={() => {
+            logOut();
+            router.push("/auth/login");
+          }}
+        >
+          <LogOutIcon className="size-5 text-destructive" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
